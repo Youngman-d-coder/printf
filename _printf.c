becd7028fb2a5c70c1ae47781e631a9b7b4ddb86
Bobbyzain <abhameed419@gmail.com>
2023-06-25 12:33:39 -0700

    adding file Co-authored-by: Nelson <sayhitonelson@gmail.com>

--- _printf.c
+++ _printf.c
@@ -12,7 +12,50 @@
 int _printf(const char *format, ...)
 {
 	va_list ap;
+	int written = 0;
+	va_start(ap, format);
 	int i;
 
-	va_start(ap, format);
+	for (const char *c = format; *c != '\0'; c++)
+	{
+		if (*c == '%')
+		{
+			/* Move past the '%' */
+			c++;
+
+			if (*c == '\0')
+			{
+				/* Unexpected end of format string */
+				break;
+			}
+
+			if (*c ==  '%')
+			{
+				/* Handle "%%" case */
+				write(1, "%", 1);
+				written++;
+			}
+			else if (*c == 's')
+			{
+				/* Handle string format specifier */
+				char *str = va_arg(ap, char*);
+				for (i = 0; str[i] != '\0'; i++)
+				written += write(1, str, i);
+			}
+			else
+			{
+				/* Unsupported format specifier, ignore */
+			}
+		}
+		else
+		{
+			/* Regular character, write it to stdout */
+			write(1, c, 1);
+			written++;
+		}
+
+	}
+	va_end(ap);
+
+	return (written);
 }
