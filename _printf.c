c45501561d6880ad1e170e017c9ccbf8236844d9
Bobbyzain <abhameed419@gmail.com>
2023-06-25 11:29:42 -0700

    The printf c file  for the first task

---
+++ _printf.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+/**
+ * _printf - Our printf function
+ * @format: Specifying the format expected in standard output
+ *
+ * Return: Always 0
+ */
+
+int _printf(const char *format, ...)
+{
+	va_list ap;
+	int i;
+
+	va_start(ap, format);
+}
