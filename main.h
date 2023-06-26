ade8a73a9e768d3484c9f275ac9b61e51a3de846
Nelson <sayhitonelson@gmail.com>
2023-06-26 01:29:21 +0100

    adding file Co-authored-by: Bobbyzain <abu.abdulhameed@outlook.com>

--- main.h
+++ main.h
@@ -9,7 +9,7 @@
 int _printf(const char *format, ...);
 int strings(int written, va_list ap);
 int integer_counter(int num);
-char* int_to_str(int num);
+char *int_to_str(int num);
 int int_spec(int written, va_list ap);
 
 
