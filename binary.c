df46b5945b4538be06ac5f305547679cbb215bb5
Nelson <sayhitonelson@gmail.com>
2023-06-27 00:11:49 +0100

    adding file Co-authored-by: Bobbyzain <abu.abdulhameed@outlook.com>

--- binary.c
+++ binary.c
@@ -14,13 +14,11 @@
  */
 int dec_to_bin(int written, va_list ap)
 {
-	int ans, len;
+	int ans, i = 0, j, temp;
 	int bin = va_arg(ap, int); 
 	char *buffer;
-	int *ptr;
 
 
-	buffer = int_to_str(ans);
 	while (bin > 0)
 	{
 		ans = bin % 2;
@@ -29,9 +27,21 @@ int dec_to_bin(int written, va_list ap)
 	/**	if (buffer == NULL)
 	 *	return (written);
 	 */
-		len = integer_counter(bin);
-		written += write(1, buffer, len);
+		buffer = int_to_str(ans);
+		i += write(1, buffer, 1);
 	}
+
+	j = i;
+
+	while (j > i)
+	{
+		temp = buffer[i];
+		buffer[i] = buffer[j];
+		buffer[j] = temp;
+		i++;
+		j--;
+	}
+	written += j;
         free(buffer);
         return (written);
 }
